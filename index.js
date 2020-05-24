8e62da7825050c9d028b826f2c8c1cc554aadf05
jonasnoki <jonasnoackgruen@gmail.com>
2020-05-24 02:00:22 +0200

    Add multi month functionality.

--- index.js
+++ index.js
@@ -2,52 +2,84 @@ const puppeteer = require('puppeteer');
 const { Telegraf } = require('telegraf');
 require('dotenv').config();
 
-const refreshTimeInSeconds = 30;
+const refreshTimeInSeconds = 2 * 60;
+const numberOfMonthToCheck = 3;
 
 (async () => {
   const browser = await puppeteer.launch({args: ['--no-sandbox']});//{headless: false});
+  let lastMessage = "";
 
-  const chatIds = [];
+  const chats = [];
   const bot = new Telegraf(process.env.BOT_TOKEN)
   bot.start(async (ctx) => {
-    ctx.reply("Welcome to the Barberini tickets bot. I'll notify you when there are new Tickets available fo you!");
-    console.log('ctx: ', ctx);
+    ctx.reply("Willkommen beim Barberini Ticket Warnservice. Ich sende dir eine Nachricht sobald neue Tickets verfügbar sind!");
     const chat = await ctx.getChat();
-    chatIds.push(chat.id);
-    console.log('added chat id: ', chat.id);
+    chats.push({lastMessage: "", chatId: chat.id});
+    console.log('added chat: ', chat);
   })
   bot.hears('hi', (ctx) => ctx.reply('Hey there'))
   bot.launch()
 
   checkForTickets = async ()=> {
+    console.log("Checking for Tickets...")
     const page = await browser.newPage();
     await page.goto('https://shop.museum-barberini.com/#/tickets');
-    
-    page.on('response', response => {
+    await page.setDefaultNavigationTimeout(0);
+
+    const availableDates = [];
+
+    const clickNextMonth = async () => {
+      await page.click('.barberini-tickets .day-selection .uib-right');
+    };
+
+    let monthIndex = 0;
+
+    page.on('response', async response => {
       if (response.url().includes("/tickets/calendar")) {
-        response.text().then(function (text) {
+        response.text().then(async (text) => {
           
           const responseObj = JSON.parse(text).data;
   
-          const availableDates = [];
           Object.keys(responseObj).forEach(function(date) {
             var ticketsAvailable = responseObj[date];
-            console.log('ticketsAvailable: ',ticketsAvailable);
-            console.log('date: ',date);
   
             if(!!ticketsAvailable){
               availableDates.push(date);
             }
           });
-  
-          if(availableDates.length > 0){
-            chatIds.forEach((chatId) => {
-              bot.telegram.sendMessage(chatId, "There are now Tickets available for the following Dates: " + availableDates);
-              bot.telegram.sendMessage(chatId, "Just go to https://shop.museum-barberini.com/#/tickets to book your Tickets!");
-            })
+
+          if(monthIndex < numberOfMonthToCheck){
+            monthIndex++;
+            await clickNextMonth();
           }
-  
-          page.close()
+          
+          if (monthIndex === numberOfMonthToCheck){
+            let newMessage = `
+Es gibt jetzt wieder Tickets für folgende Tage: ${availableDates}.
+Klicke hier um Tickets zu buchen: https://shop.museum-barberini.com/#/tickets`;
+      
+            if(availableDates.length > 0){
+              const idsSent = [];
+              chats.forEach(({lastMessage, chatId}, chatIndex) => {
+                if(newMessage !== lastMessage){
+                  idsSent.push(chatId);
+                  bot.telegram.sendMessage(chatId, newMessage);
+                  chats[chatIndex].lastMessage = newMessage;
+                }
+              })
+
+              if (idsSent.length > 0){
+                console.log(`
+Sending Message: 
+'${newMessage}' 
+To chat ids: ${idsSent}
+                `);
+              }
+              
+            }
+
+            page.close();
+          }
   
         }).catch((e)=>{
           // silently fail
